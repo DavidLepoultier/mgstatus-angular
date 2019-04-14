4e336602d23f5cd1cbe33e0c557c35ce33bb4a1d
David Lepoultier <david.lepoultier@gmail.com>
2019-04-14 19:32:08 +0200

    Update project-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,8 @@ import { ChartsModule } from 'ng2-charts';
 import { NavigationComponent } from './navigation/navigation.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { PipesPipe } from './pipes/pipes.pipe';
+import { DowntimePipe } from './pipes/downtime.pipe';
 
 const appRoutes: Routes = [
   {
@@ -45,7 +46,9 @@ const appRoutes: Routes = [
     ProjectDetailComponent,
     NavigationComponent,
     DashboardComponent,
-    FooterComponent
+    FooterComponent,
+    PipesPipe,
+    DowntimePipe
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
