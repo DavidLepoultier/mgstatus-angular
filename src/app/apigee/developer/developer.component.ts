7a59f4780ea0fa727a73ca6175ef933e39b67c37
LEPOULTIER David TGI/OLS <CSRX8264@orange.com>
2019-04-24 19:06:57 +0200

    Update

--- src/app/apigee/developer/developer.component.ts
+++ src/app/apigee/developer/developer.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
 import { ApigeeService } from '../apigee.service';
-import { NotifierSvc } from '../../services/notifier.service'
+import { NotifierSvc } from '../../services/notifier.service';
+import { MdbTableService } from 'angular-bootstrap-md';
 
 
 @Component({
@@ -11,16 +12,21 @@ import { NotifierSvc } from '../../services/notifier.service'
   styleUrls: ['./developer.component.scss']
 })
 export class DeveloperComponent implements OnInit {
-
   notifier: NotifierSvc;
   developer: object = {
-    developer: 'david.lepoultier@gmail.com',
+    developer: 'david.lepoultier@orange.com',
   }
-  myApps: any = null;
+  myApps: any = [];
+  private sorted = true;
+  searchText: string = '';
+  previous: string;
 
-
-  constructor(private router:Router, private auth:AuthService, private apigee:ApigeeService, notifierSvc:NotifierSvc ) {
+  constructor(private router:Router, private auth:AuthService, private apigee:ApigeeService, notifierSvc:NotifierSvc, private mdbTable:MdbTableService ) {
     this.notifier = notifierSvc;
+  }
+
+  @HostListener('input') oninput() {
+    this.searchItems();
   }
 
   ngOnInit() {
@@ -28,6 +34,38 @@ export class DeveloperComponent implements OnInit {
       this.router.navigate(['/']);
     }
     this.getDeveloperApps(this.developer);
+  }
+
+  sortBy(by: string | any): void {
+
+    this.myApps.sort((a: any, b: any) => {
+      if (a[by] > b[by]) {
+        return this.sorted ? -1 : 1;
+      }
+      if (a[by] < b[by]) {
+        return this.sorted ? 1 : -1;
+      }
+
+      return 0;
+    });
+
+    this.sorted = !this.sorted;
+  }
+
+  searchItems() {
+    const prev = this.mdbTable.getDataSource();
+    
+
+    if (!this.searchText) {
+      this.mdbTable
+      this.mdbTable.setDataSource(this.previous);
+      this.myApps = this.mdbTable.getDataSource();
+    }
+
+    if (this.searchText) {
+      this.myApps = this.mdbTable.searchLocalDataBy(this.searchText);
+      this.mdbTable.setDataSource(prev);
+    }
   }
 
   getDeveloperApps(dev: object) {
@@ -49,7 +87,29 @@ export class DeveloperComponent implements OnInit {
   }
 
   handlerServerResponse(data: any) {
-    this.myApps = JSON.stringify(data);
+    for (let index = 0; index < data.developerApps.app.length; index++) {
+      let approved = false;
+      if(data.developerApps.app[index].status === 'approved')
+        approved = true
+      this.myApps.push({
+        devApp: data.developerApps.app[index].name,
+        status: data.developerApps.app[index].status,
+        approved: approved
+      });
+    }
+    this.myApps = this.myApps.sort((a: any, b: any) => {
+      if (a['devApp'] < b['devApp']) {
+        return this.sorted ? -1 : 1;
+      }
+      if (a['devApp'] > b['devApp']) {
+        return this.sorted ? 1 : -1;
+      }
+
+      return 0;
+    });
+
+    this.mdbTable.setDataSource(this.myApps);
+    this.previous = this.mdbTable.getDataSource();
   }
   
 }
